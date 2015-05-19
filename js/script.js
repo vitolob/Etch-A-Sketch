cf9247e8997ad721d4de8929a48073ad745838b9
Victorino Machava <vitomachava@gmail.com>
2015-05-19 23:22:57 +0200

    changed from hover to mouseenter method

--- js/script.js
+++ js/script.js
@@ -2,11 +2,8 @@ var main = function() {
 	for (var i = 0; i < 256; i++) {
 		$("<div></div>", {class: "grid"}).appendTo("div.container");
 	};
-	$(".grid").hover(function() {
+	$(".grid").mouseenter(function() {
 		$(this).css("background-color", "#eeddbb");
-	}, 
-	function() {
-		$(this).css("background-color", "#ffbbcc");
 	});
 
 };
